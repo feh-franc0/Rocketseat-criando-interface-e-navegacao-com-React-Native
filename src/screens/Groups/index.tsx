f01a3c91090ffc9f2060bd2783010ae7cb99be21
feh-franc0 <fernandofv1110@gmail.com>
2023-08-16 16:31:10 -0300

    Criando Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
@@ -11,6 +12,8 @@ export function Groups() {
         title='Turmas'
         subtitle='Jogue com a sua turma'
       />
+
+      <GroupCard title='Galera do Ignite' />
     </Container>
   );
 }
