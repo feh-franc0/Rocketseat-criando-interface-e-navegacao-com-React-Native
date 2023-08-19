0aac29dce1ec6b7bc378dbe6d09d6f9488bb0137
feh-franc0 <fernandofv1110@gmail.com>
2023-08-19 19:53:34 -0300

    Prevenindo Glitch na navegação

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,7 +8,7 @@ const {Navigator, Screen} = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{headerShown: false}}>
+    <Navigator screenOptions={{headerShown: false, animation: "fade_from_bottom"}}>
       <Screen 
         name="groups"
         component={Groups}
