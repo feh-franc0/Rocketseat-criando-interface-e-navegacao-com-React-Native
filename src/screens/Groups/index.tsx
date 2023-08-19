bcaa986ac130765e93970c6c73c0aa2374c15261
feh-franc0 <fernandofv1110@gmail.com>
2023-08-19 18:23:55 -0300

    Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 import {FlatList} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
 
 import { Header } from '@components/Header';
-import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+import { Container } from './styles';
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
-  function handleNewGroup() {
+  const navigation = useNavigation()
 
+  function handleNewGroup() {
+    navigation.navigate('new')
   }
   
   return (
